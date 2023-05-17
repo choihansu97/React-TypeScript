9d4e3a2998ed307dce06a5e0aad769b0b187971a
choihansu97 <60763027+choihansu97@users.noreply.github.com>
2023-05-18 00:34:12 +0900

    feat(data): 목데이터 제작

--- src/index.tsx
+++ src/index.tsx
@@ -2,18 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import * as D from './data'
+
+const childrend = D.makeArray(10).map((notUsed, index) => (
+  <div key={index}>
+    <p>{D.randomId()}</p>
+    <p>{D.randomName()}</p>
+    <p>{D.randomJobTitle()}</p>
+  </div>
+))
+
+const rootVisualDom = <div>{childrend}</div>
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    rootVisualDom
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
